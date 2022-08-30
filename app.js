154f3bd83719c408f17c0cb8239d27d622f00005
ryan phan <ryanphan@ryans-MacBook-Pro.local>
2022-08-29 20:33:42 -0400

    made a reset button and added text content

---
+++ app.js
@@ -0,0 +1,4 @@
+const gridBasic = document.querySelector('.gridBasic');
+const gridEraser = document.querySelector('.gridEraser')
+const gridColor = document.querySelector('.gridColor')
+const gridClear = document.querySelector('.gridClear')
